cfc34fc5b08119c7d7d2565e95811ba0966d3644
Miha Zupan <mihazupan.zupan1@gmail.com>
2026-05-15 02:28:37 +0200

    Try to avoid extra parent dir when zipping on linux

--- Runner/JobBase.cs
+++ Runner/JobBase.cs
@@ -249,10 +249,8 @@ public abstract class JobBase
         else
         {
             folderPath = Path.GetFullPath(folderPath);
-            string workDir = Path.GetDirectoryName(folderPath) ?? throw new Exception($"No parent folder for '{folderPath}'?");
-            folderPath = Path.GetRelativePath(workDir, folderPath);
 
-            await RunProcessAsync("zip", $"-{(maxCompression ? 6 : 3)} -r {zipFilePath} {folderPath}", logPrefix: zipFileName, workDir: workDir, suppressOutputLogs: true);
+            await RunProcessAsync("zip", $"-{(maxCompression ? 6 : 3)} -r {zipFilePath} .", logPrefix: zipFileName, workDir: folderPath, suppressOutputLogs: true);
         }
 
         await UploadArtifactAsync(zipFilePath);
